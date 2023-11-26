a81be50efca3b19b0900c8b5464ddc10ea25d87e
Gary Bouvet <126561503+garybouvet@users.noreply.github.com>
2023-11-26 08:45:03 +0100

    Update 1_Accueil.py

--- 1_Accueil.py
+++ 1_Accueil.py
@@ -35,13 +35,25 @@ st.markdown(
 
 # Afficher le logo centré avec la description en dessous
 st.markdown(
-    f"""
+    <style>
+        .centered-image {
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+            align-items: center;
+            text-align: center;
+        }
+        .custom-heading {
+            font-family: "Helvetica Neue", sans-serif;
+            font-size: 24px;
+            font-weight: bold;
+            margin-top: 10px;
+        }
+    </style>
+
     <div class="centered-image">
         <img src="data:image/png;base64,{resized_logo_base64}" alt="Tomme Creuse - Mission possible">
-        <h1 style="margin-top: 40px;">Collecte et nettoyage de données depuis IMDB</h1>
-        <h1>Enrichissement automatisé à partir de TMDB via API</h1>
-        <h1>Développement d'un moteur de Machine Learning (Cosine similarity) pour des recommandations de films</h1>
+        <h1 class="custom-heading">Collecte et nettoyage de données depuis IMDB</h1>
+        <h1 class="custom-heading">Enrichissement automatisé à partir de TMDB via API</h1>
+        <h1 class="custom-heading">Développement d'un moteur de Machine Learning (Cosine similarity) pour des recommandations de films</h1>
     </div>
-    """,
-    unsafe_allow_html=True
-)
