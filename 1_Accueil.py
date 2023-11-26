d9a5246b97ae525616f1df07a821d183640827eb
Gary Bouvet <126561503+garybouvet@users.noreply.github.com>
2023-11-26 09:11:06 +0100

    Update 1_Accueil.py

--- 1_Accueil.py
+++ 1_Accueil.py
@@ -1,54 +1,32 @@
 import streamlit as st
-import base64
-from PIL import Image
-from io import BytesIO
 
 st.set_page_config(
     page_title="MGC Cinéma",
     layout="wide"
 )
 
-logo_path = "./Logo/logo_tomme_creuse.png"
-logo_image = Image.open(logo_path)
-resized_logo = logo_image.resize((300, 300))
+logo_path = "./Logo/MGC_Cinema_Logo.png"
 
-# Convertir l'image redimensionnée en base64
-buffered = BytesIO()
-resized_logo.save(buffered, format="PNG")
-resized_logo_base64 = base64.b64encode(buffered.getvalue()).decode()
-
-# Centrer le logo
+# Center the logo
 st.markdown(
-    f"""
+    """
     <style>
-    .centered-image {{
+    .centered-image {
         display: flex;
         justify-content: center;
-        flex-direction: column;
         align-items: center;
         text-align: center;
-    }}
-    .custom-list {{
-        text-align: left;
-        margin-top: 40px;
-        line-height: 1.2;  /* Adjust line spacing as needed */
-        font-size: 18px;  /* Adjust font size as needed */
-    }}
+    }
     </style>
     """,
     unsafe_allow_html=True
 )
 
-# Afficher le logo centré avec la description en dessous
+# Display the centered logo at its original size without text
 st.markdown(
     f"""
     <div class="centered-image">
-        <img src="data:image/png;base64,{resized_logo_base64}" alt="Tomme Creuse - Mission possible">
-        <ul class="custom-list">
-            <li> Collecte et nettoyage de données depuis IMDB</li>
-            <li> Enrichissement automatisé à partir de TMDB via API</li>
-            <li> Développement d'un moteur de Machine Learning (Cosine similarity) pour des recommandations de films</li>
-        </ul>
+        <img src="{logo_path}" alt="Tomme Creuse - Mission possible">
     </div>
     """,
     unsafe_allow_html=True
